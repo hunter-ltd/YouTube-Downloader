792597da5be2342d1143910d082d363dc6cf6597
dablenparty <hdavenport8902@gmail.com>
2021-04-14 00:30:28 -0400

    Added compile.js which does as the name implies: compiles the typescript

---
+++ src/compile.js
@@ -0,0 +1,17 @@
+const os = require('os');
+const { exec } = require('child_process');
+
+// TODO: Add MacOSX and Linux (look here for values: https://stackoverflow.com/questions/45082648/npm-package-json-os-specific-script)
+const compile = () => {
+    let command = "tsc && "
+    switch (os.type()) {
+        case "Windows_NT":
+            command += "robocopy src build *.html *.css";
+            break;
+        default:
+            throw new Error("Unsupported OS: " + os.type());
+    }
+    exec(command);
+}
+
+compile();
