d8985266df869b428abe38302a1208b6ec4d0ce7
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-04-15 14:56:23 -0400

    Added optional fileName parameter to save method

--- src/ts/video.ts
+++ src/ts/video.ts
@@ -1,6 +1,7 @@
 import * as ffmpeg from "fluent-ffmpeg";
 import {path as ffmpegPath} from "@ffmpeg-installer/ffmpeg";
 import * as ytdl from "ytdl-core";
+import {join} from "path";
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -13,14 +14,17 @@ export class YouTubeVideo {
     /**
      * Downloads and saves the video from YouTube
      * @param path Where to save the video
+     * @param fileName Optional file name (otherwise assumed to be included in path)
      */
-    public save = async (path: string) => {
+    public save = async (path: string, fileName?: string) => {
+        path = fileName === undefined ? path : join(path, fileName);
         return new Promise(async (resolve, reject) => {
             let stream = ytdl(this._url, {filter: "audioonly"}),
                 output = ffmpeg(stream).save(path);
             [stream, output].forEach(item => item.on('error', err => reject(err)));
             output.on('start', () => {
                 // TODO: set start status
+                console.log("downloading...")
             }).on('end', () => resolve(path)); // TODO: resolve with a custom audio file class later on
         });
     }
