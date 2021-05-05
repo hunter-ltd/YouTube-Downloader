3b0d51d2ae8ecbe0b252a370b06fcc8f36f54f32
dablenparty <hdavenport8902@gmail.com>
2021-05-05 00:31:29 -0400

    Added progress reporting in the gui in text form

--- src/ts/video.ts
+++ src/ts/video.ts
@@ -20,16 +20,18 @@ export class YouTubeVideo {
     public save = async (path: string, fileName?: string) => {
         path = fileName === undefined ? path : join(path, fileName);
         return new Promise(async (resolve, reject) => {
-            let stream = ytdl(this._url, {filter: "audioonly"}),
-                output = ffmpeg(stream)
+            let stream = ytdl(this._url, {filter: "audioonly"})
+                .on('error', err => reject(err))
+                .on('progress', (_, current, total) => {
+                    document.getElementById('status').innerHTML = `${Math.round(100 * (current / total))} % downloaded`
+                })
+                ffmpeg(stream)
                     .on('start', () => {
-                    document.getElementById('status').innerHTML = 'Downloading...';
-                    console.log(`Downloading ${this._url} >> ${path}`);
+                    console.log(`ffmpeg started: ${this._url} >> ${path}`);
                 })
                     .on('end', () => resolve(new AudioFile(path)))
+                    .on('error', err => reject(err))
                     .save(path);
-
-            [stream, output].forEach(item => item.on('error', err => reject(err)));
         });
     }
 }
