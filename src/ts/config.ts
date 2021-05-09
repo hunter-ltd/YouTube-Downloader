1d13a1704dfd6b81e16b0f41363e681e95415eb6
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-08 22:48:06 -0400

    Fixed type issues with the config data

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,50 +1,54 @@
 import * as path from "path";
 import * as fs from "fs";
-import {ipcRenderer} from "electron";
+import { ipcRenderer } from "electron";
+
+interface ConfigData {
+  savePath: string;
+}
 
 export class UserConfig {
-    private readonly _path: string;
-    private _data: Object;
-    private _savePath: string;
+  private readonly _path: string;
+  private _data: ConfigData;
+  private _savePath: string;
 
-    set data(value: Object) {
-        this._data = value;
-        this._savePath = value['savePath'];
-    }
+  set data(value: ConfigData) {
+    this._data = value;
+    this._savePath = value["savePath"];
+  }
 
-    get path(): string {
-        return this._path;
-    }
+  get path(): string {
+    return this._path;
+  }
 
-    get savePath(): string {
-        return this._savePath;
-    }
+  get savePath(): string {
+    return this._savePath;
+  }
 
-    set savePath(path: string) {
-        this._savePath = path;
-        this._data['savePath'] = path;
-        fs.writeFileSync(this.path, JSON.stringify(this._data));
-    }
+  set savePath(path: string) {
+    this._savePath = path;
+    this._data["savePath"] = path;
+    fs.writeFileSync(this.path, JSON.stringify(this._data));
+  }
 
-    constructor(path: string) {
-        this._path = path;
-    }
+  constructor(path: string) {
+    this._path = path;
+  }
 
-    public parseDataFile = () => {
-        return new Promise<Object>((resolve, reject) => {
-            try {
-                resolve(JSON.parse(fs.readFileSync(this.path).toString()));
-            } catch (e) {
-                ipcRenderer.invoke("getPath", "downloads").then(result => {
-                    let data: Object = {
-                        savePath: result
-                    };
-                    fs.writeFileSync(this.path, JSON.stringify(data)); // creates and writes to the settings file
-                    resolve(data);
-                });
-            }
+  public parseDataFile = () => {
+    return new Promise<ConfigData>((resolve, _) => {
+      try {
+        resolve(JSON.parse(fs.readFileSync(this.path).toString()));
+      } catch (e) {
+        ipcRenderer.invoke("getPath", "downloads").then((result) => {
+          let data: ConfigData = {
+            savePath: result,
+          };
+          fs.writeFileSync(this.path, JSON.stringify(data)); // creates and writes to the settings file
+          resolve(data);
         });
-    }
+      }
+    });
+  };
 }
 
 /**
@@ -52,15 +56,18 @@ export class UserConfig {
  * @returns {Promise<UserConfig>} A promise resolving with the UserConfig object and rejecting with an error
  */
 export async function makeNewConfig(): Promise<UserConfig> {
-    return new Promise<UserConfig>((resolve, reject) => {
-        ipcRenderer.invoke("getPath", "userData").then(async value => {
-            let config = new UserConfig(path.resolve(value, "userSettings.json"));
-            await config.parseDataFile().then(value => {
-                config.data = value;
-                resolve(config)
-            });
-        }).catch(err => {
-            reject(err);
+  return new Promise<UserConfig>((resolve, reject) => {
+    ipcRenderer
+      .invoke("getPath", "userData")
+      .then(async (value: string) => {
+        let config = new UserConfig(path.resolve(value, "userSettings.json"));
+        await config.parseDataFile().then((value) => {
+          config.data = value;
+          resolve(config);
         });
-    });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
