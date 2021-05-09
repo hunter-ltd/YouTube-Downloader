6ba4c95b9b9d88464c8827757c0718a51c1dc534
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-08 20:52:05 -0400

    Refactored compile functions return promise

--- src/js/compile.js
+++ src/js/compile.js
@@ -1,35 +1,44 @@
-const { readdir, mkdir, copyFile } = require('fs').promises;
-const { resolve, dirname, basename } = require('path');
-const { exec } = require('child_process');
+const { readdir, mkdir, copyFile } = require("fs").promises;
+const { resolve, dirname, basename } = require("path");
+const { exec } = require("child_process");
 
 // a recursive generator that basically eliminates callback hell (cough stackoverflow)
 async function* getAllFiles(dir) {
-    const items = await readdir(dir, { withFileTypes: true }); // gets all the items in the current dir
-    for (const item of items) {
-        const res = resolve(dir, item.name);
-        if (item.isDirectory()) {
-            yield* getAllFiles(res); // yields a new generator created from the given directory
-        } else {
-            yield res; // yields the file path
-        }
+  const items = await readdir(dir, { withFileTypes: true }); // gets all the items in the current dir
+  for (const item of items) {
+    const res = resolve(dir, item.name);
+    if (item.isDirectory()) {
+      yield* getAllFiles(res); // yields a new generator created from the given directory
+    } else {
+      yield res; // yields the file path
     }
+  }
 }
 
-const compile = async () => {
+const compile = () => {
+  return new Promise(async (resolvePromise, rejectPromise) => {
     exec("tsc");
     for await (const file of getAllFiles(resolve(process.cwd(), "src"))) {
-        let parent = dirname(file);
-        if (!(parent.endsWith("ts") || parent.endsWith("js"))) {
-            mkdir(resolve(process.cwd(), "build", basename(parent)), { recursive: true }).finally(() => {
-                // I want the files copied regardless of if the directory exists or not. If there is a more severe
-                // error that rejects the copying, then that will reject this function with the same error
-                copyFile(file, resolve(process.cwd(), "build", basename(parent), basename(file))).catch(err => {
-                    return new Promise((resolve1, reject) => reject(err));
-                });
-            });
-        }
+      let parent = dirname(file);
+      if (!(parent.endsWith("ts") || parent.endsWith("js"))) {
+        mkdir(resolve(process.cwd(), "build", basename(parent)), {
+          recursive: true,
+        }).finally(() => {
+          // I want the files copied regardless of if the directory exists or not. If there is a more severe
+          // error that rejects the copying, then that will reject this function with the same error
+          copyFile(
+            file,
+            resolve(process.cwd(), "build", basename(parent), basename(file))
+          ).catch((err) => {
+            rejectPromise(err);
+          });
+        });
+      }
     }
-    return new Promise(resolve => resolve());
-}
+    resolvePromise();
+  });
+};
 
-compile().then(() => console.log('Electron TypeScript successfully compiled')).catch(err => console.error(err));
+compile()
+  .then(() => console.log("Electron TypeScript successfully compiled"))
+  .catch((err) => console.error(err));
