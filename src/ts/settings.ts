2e7c7572df31bacd5fbb2252ec679414f6c00dde
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-05-25 00:04:15 -0400

    Refactored settings.ts to use async/await

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -1,60 +1,60 @@
-import {makeNewConfig} from "./config";
-import {ipcRenderer} from "electron";
-import {existsSync} from "fs";
+import { makeNewConfig, UserConfig } from "./config";
+import { ipcRenderer } from "electron";
+import { existsSync } from "fs";
 
 class SettingsMenu {
-    set config(value) {
-        this._config = value;
-    }
-    get config() {
-        return this._config;
-    }
+  set config(value) {
+    this._config = value;
+  }
+  get config() {
+    return this._config;
+  }
 
-    get folderElement(): HTMLInputElement {
-        return this._folderElement;
-    }
+  get folderElement(): HTMLInputElement {
+    return this._folderElement;
+  }
 
-    get saveBtn(): HTMLButtonElement {
-        return this._saveBtn;
-    }
+  get saveBtn(): HTMLButtonElement {
+    return this._saveBtn;
+  }
 
-    get selectBtn(): HTMLLabelElement {
-        return this._selectBtn;
-    }
-    private _config;
-    private readonly _folderElement: HTMLInputElement;
-    private readonly _saveBtn: HTMLButtonElement;
-    private readonly _selectBtn: HTMLLabelElement;
-    constructor() {
-        this._folderElement = <HTMLInputElement>document.getElementById('save-folder');
-        this._saveBtn = <HTMLButtonElement>document.getElementById('save-btn');
-        this._selectBtn = <HTMLLabelElement>document.getElementById('save-folder-label'); // makes the label the button
-    }
+  get selectBtn(): HTMLLabelElement {
+    return this._selectBtn;
+  }
+  private _config: UserConfig;
+  private readonly _folderElement: HTMLInputElement;
+  private readonly _saveBtn: HTMLButtonElement;
+  private readonly _selectBtn: HTMLLabelElement;
+  constructor() {
+    this._folderElement = <HTMLInputElement>(
+      document.getElementById("save-folder")
+    );
+    this._saveBtn = <HTMLButtonElement>document.getElementById("save-btn");
+    this._selectBtn = <HTMLLabelElement>(
+      document.getElementById("save-folder-label")
+    ); // makes the label the button
+  }
 
-    public initialize = async () => {
-        await makeNewConfig().then(config => {
-            this._config = config;
-            this.folderElement.value = this.config.savePath;
-        });
+  public initialize = async () => {
+    this._config = await makeNewConfig();
+    this.folderElement.value = this.config?.savePath;
 
-        this.selectBtn.addEventListener('click', ev => {
-            ipcRenderer.invoke("showOpenDialog").then(async (result: Electron.OpenDialogReturnValue) => {
-                // While the function being called in main.ts technically returns a promise, it automatically resolves
-                // with the resulting object
-                if (!result.canceled) {
-                    let path: string = result.filePaths[0];
-                    this.folderElement.value = path;
-                    this.config.savePath = path;
-                }
-            });
-        });
+    this.selectBtn.addEventListener("click", async (_) => {
+      const result: Electron.OpenDialogReturnValue = await ipcRenderer.invoke(
+        "showOpenDialog"
+      );
+      if (result.canceled) return;
+      let path: string = result.filePaths[0];
+      this.folderElement.value = path;
+      this.config.savePath = path;
+    });
 
-        this.saveBtn.addEventListener('click', ev => {
-            let newPath = this.folderElement.value;
-            if (existsSync(newPath)) this.config.savePath = newPath;
-        })
-        return this;
-    }
+    this.saveBtn.addEventListener("click", (_) => {
+      let newPath = this.folderElement.value;
+      if (existsSync(newPath)) this.config.savePath = newPath;
+    });
+    return this;
+  };
 }
 
 new SettingsMenu().initialize();
